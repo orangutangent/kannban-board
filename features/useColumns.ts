85b01fe0b13f91d81c4fcf0e1def47ef9ded2e8a
Denis <145012169+orangutangent@users.noreply.github.com>
2024-05-09 01:09:00 +0700

    Update useColumns.ts

--- features/useColumns.ts
+++ features/useColumns.ts
@@ -7,20 +7,39 @@ type Column = {
     color: string
 }
 
-const useColumns = create((set) => ({
+type UseColumnsInterface = {
+    columns: Column[]
+    fetchColumns: () => void
+    addColumn: (column: Column) => void
+    deleteColumn: (id: string) => void
+    updateColumn: (column: Column) => void
+}
+
+const fetchColumns = async () => {
+    const { data } = await axios.get('/api/columns')
+    return data
+}
+
+const useColumns = create<UseColumnsInterface>((set) => ({
     columns: [],
     fetchColumns: async () => {
-        const { data } = await axios.get('/api/columns')
+        const data = await fetchColumns()
         set({ columns: data })
     },
     addColumn: async (column: Column) => {
-        await axios.post('/api/columns', column)
+        await axios.post('https://663baf1ffee6744a6ea2910b.mockapi.io/columns', column)
+        const data = await fetchColumns()
+        set({ columns: data })
     },
     deleteColumn: async (id: string) => {
-        await axios.delete(`/api/columns/${id}`)
+        await axios.delete(`/https://663baf1ffee6744a6ea2910b.mockapi.io/columns/${id}`)
+        const data = await fetchColumns()
+        set({ columns: data })
     },
     updateColumn: async (column: Column) => {
-        await axios.put(`/api/columns/${column.id}`, column)
+        await axios.put(`https://663baf1ffee6744a6ea2910b.mockapi.io/columns/${column.id}`, column)
+        const data = await fetchColumns()
+        set({ columns: data })
     },
 
 }))
