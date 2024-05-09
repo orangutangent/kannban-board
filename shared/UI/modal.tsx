ab0689805f28cce8a27f618e0a7d48aeaeeb1827
YourMori <pa4ok_28@mail.ru>
2024-05-09 21:15:15 +0700

    first commit

--- shared/UI/modal.tsx
+++ shared/UI/modal.tsx
@@ -1,23 +1,13 @@
-
-
 interface Props {
-    title?: string;
-    body?: React.ReactNode;
-    onClose: () => void;
-    onSubmit: () => void;
-    actionLabel: string;
+	title?: string;
+	body?: React.ReactNode;
+	onClose: () => void;
+	onSubmit: () => void;
+	actionLabel: string;
 }
 
-const Modal: React.FC<Props> = ({
-    title,
-    body,
-    onClose,
-    onSubmit,
-    actionLabel
-}) => {
-    return ( <div>
-        
-    </div> );
-}
- 
+const Modal: React.FC<Props> = ({ title, body, onClose, onSubmit, actionLabel }) => {
+	return <div></div>;
+};
+
 export default Modal;
