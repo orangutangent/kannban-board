a5639e06201bc71ba45ed2883189948b1a177e35
JuliaYarlykova <yarlykova2506@gmail.com>
2024-05-10 23:34:44 +0700

    buttons: delete and create column

--- widgets/column.tsx
+++ widgets/column.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 
 import useCards, { ICard } from '@/features/useCards';
 import useColumns, { IColumn } from '@/features/useColumns';
+import trashcan from '@/public/icons/trashcan.svg';
 import Card1 from '@/shared/UI/card';
 import edit from '../public/icons/edit.svg';
 import plus from '../public/icons/plus.svg';
@@ -57,7 +58,7 @@ export default function Column(column: IColumn) {
 
 	return (
 		<li
-			className='custom-column relative list-none w-[256px] bg-none z-0 min-w-[208px]'
+			className='custom-column relative list-none w-[256px] bg-none z-0 min-w-[220px]'
 			onMouseEnter={() => setIsHovered(true)}
 			onMouseLeave={() => setIsHovered(false)}
 			draggable={true}
@@ -66,17 +67,40 @@ export default function Column(column: IColumn) {
 			onDragStart={(e) => dragStartHandler(e, column)}
 			onDrop={(e) => dropHandler(e, column)}
 		>
-			<h2 className='text-2xl font-bold mb-[12px] lg:text-xl md:text-base '>{column.title}</h2>
+			<h2 className='text-2xl font-bold mb-[12px] lg:text-xl md:text-base '>
+				{column.title}
+			</h2>
 			{isHovered && (
 				<div className='flex gap-[8px] absolute right-[6px] top-[6px]'>
-					<Image src={edit} alt='edit' width={16} className='cursor-pointer' onClick={() => setModalEdit(true)}></Image>
-					<Image src={plus} alt='plus' width={16} className='cursor-pointer' onClick={() => setModalAddColumn(true)}></Image>
+					<Image
+						src={trashcan}
+						alt='trashcan'
+						width={16}
+						className='cursor-pointer'
+					></Image>
+					<Image
+						src={edit}
+						alt='edit'
+						width={16}
+						className='cursor-pointer'
+						onClick={() => setModalEdit(true)}
+					></Image>
+					<Image
+						src={plus}
+						alt='plus'
+						width={16}
+						className='cursor-pointer'
+						onClick={() => setModalAddColumn(true)}
+					></Image>
 				</div>
 			)}
 			{/* {modalEdit && <Modal isOpen={} />} */}
 			{/* {modalAddColumn && <Modal />} */}
-			<ul className='border-[#D6D8DB] border p-[24px] rounded flex flex-col gap-[24px]' style={{ backgroundColor: column.color }}>
-				{ cards.map(CardMap)}
+			<ul
+				className='border-[#D6D8DB] border p-[24px] rounded flex flex-col gap-[24px]'
+				style={{ backgroundColor: column.color }}
+			>
+				{cards.map(CardMap)}
 			</ul>
 		</li>
 	);
