28a09538eac38836cfb85c7c400a7c87e4b3fcde
YourMori <pa4ok_28@mail.ru>
2024-05-10 11:32:58 +0700

    added replace columns

--- app/page.tsx
+++ app/page.tsx
@@ -6,45 +6,19 @@ import { useEffect } from 'react';
 import Column from '../widgets/column';
 
 export default function Home() {
-	const { cards, fetchCards, addCard, updateCard, deleteCard } = useCards();
 	const { columns, fetchColumns } = useColumns();
+
 	useEffect(() => {
 		fetchColumns();
 	}, []);
 
-	const onAddClick = () => {
-		addCard({ title: 'hello', description: 'world', status: 'todo', tag: [] });
-	};
-
-	const onUpdateClick = (card: ICard) => {
-		updateCard({ ...card, title: 'updated' });
-	};
 	return (
 		<main className='w-screen h-screen'>
 			<ul className='flex m-[40px] gap-[24px]'>
-				{columns.map((data, key) => (
-					<Column {...data} key={key} />
+				{columns.map((data, index) => (
+					<Column {...data} key={index} />
 				))}
 			</ul>
-
-			{/* <div className="flex gap-4 cursor-pointer">
-
-      <button onClick={fetchCards}>Fetch</button>
-      <button onClick={onAddClick}>Add</button>
-      </div>
-      <div>
-        {cards.map((card) => (
-          <div key={card.id} className="flex gap-4">
-            <button onClick={() => card.id && deleteCard(card.id)}>Delete</button>
-            <button onClick={() => card.id && onUpdateClick(card)}>Update</button>
-            <div >{card.id}</div>
-            <div >{card.title}</div>
-            <div>{card.description}</div>
-            <div>{card.status}</div>
-          </div>
-
-        ))}
-      </div> */}
 		</main>
 	);
 }
--- widgets/column.tsx
+++ widgets/column.tsx
@@ -4,41 +4,69 @@ import { useEffect, useState } from 'react';
 import Image from 'next/image';
 
 import useCards, { ICard } from '@/features/useCards';
-import { IColumn } from '@/features/useColumns';
+import useColumns, { IColumn } from '@/features/useColumns';
 import Card1 from '@/shared/UI/card';
 import edit from '../public/icons/edit.svg';
 import plus from '../public/icons/plus.svg';
 
-export default function Column({ title, color }: IColumn) {
+export default function Column(column: IColumn) {
 	const [isHovered, setIsHovered] = useState(false);
 
-	const handleMouseEnter = () => {
-		setIsHovered(true);
-	};
-
-	const handleMouseLeave = () => {
-		setIsHovered(false);
-	};
+	const { cards, fetchCards } = useCards();
+	const { columns, replaceColumn } = useColumns();
 
 	useEffect(() => {
 		fetchCards();
 	}, []);
 
 	const CardMap = (data: ICard, index: number) => {
-		return data.status === title ? <Card1 {...data} {...{ color }} key={index} /> : <></>;
+		return data.status === column.title ? <Card1 {...data} key={index} /> : <></>;
 	};
 
-	const { cards, fetchCards } = useCards();
+	const dragOverHandler = (event: React.DragEvent<HTMLLIElement>) => {
+		event.preventDefault();
+		const parent = event.currentTarget as HTMLElement;
+		parent.style.boxShadow = '4px 4px 4px gray';
+	};
+
+	const dragLeaveHandler = (event: React.DragEvent<HTMLLIElement>) => {
+		const parent = event.currentTarget as HTMLElement;
+		parent.style.boxShadow = 'none';
+	};
+
+	const dragStartHandler = (event: React.DragEvent<HTMLLIElement>, column: IColumn) => {
+		const columnIndex = columns.findIndex((col) => col.id === column.id);
+		event.dataTransfer.setData('text/plain', columnIndex.toString());
+	};
+
+	const dropHandler = (event: React.DragEvent<HTMLLIElement>, column: IColumn) => {
+		event.preventDefault();
+		const parent = event.currentTarget as HTMLElement;
+		parent.style.boxShadow = 'none';
+		const draggedColumnIndex = Number(event.dataTransfer.getData('text/plain'));
+		const columnIndex2 = columns.findIndex((col) => col.id === column.id);
+		replaceColumn(draggedColumnIndex, columnIndex2);
+	};
+
 	return (
-		<li className='relative list-none w-[256px] bg-none' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-			<h2 className='text-2xl font-bold mb-[12px]'>{title}</h2>
+		<li
+			className='custom-column relative list-none w-[256px] bg-none'
+			onMouseEnter={() => setIsHovered(true)}
+			onMouseLeave={() => setIsHovered(false)}
+			draggable={true}
+			onDragOver={(e) => dragOverHandler(e)}
+			onDragLeave={(e) => dragLeaveHandler(e)}
+			onDragStart={(e) => dragStartHandler(e, column)}
+			onDrop={(e) => dropHandler(e, column)}
+		>
+			<h2 className='text-2xl font-bold mb-[12px]'>{column.title}</h2>
 			{isHovered && (
-				<div className='flex gap-[8px] absolute right-0 top-0'>
-					<Image src={edit} alt='edit' width={16}></Image>
-					<Image src={plus} alt='plus' width={16}></Image>
+				<div className='flex gap-[8px] absolute right-[6px] top-[6px]'>
+					<Image src={edit} alt='edit' width={16} className='cursor-pointer'></Image>
+					<Image src={plus} alt='plus' width={16} className='cursor-pointer'></Image>
 				</div>
 			)}
-			<ul className='border-[#D6D8DB] border p-[24px] rounded flex flex-col gap-[24px]' style={{ backgroundColor: color }}>
+			<ul className='border-[#D6D8DB] border p-[24px] rounded flex flex-col gap-[24px]' style={{ backgroundColor: column.color }}>
 				{cards.map(CardMap)}
 			</ul>
 		</li>
